4982c09b95e6a919fdbc93445b7127ecb0b75587
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-03-16 20:49:33 -0500

    added filtering by student name and event type

--- src/components/faculty/CritiqueView.vue
+++ src/components/faculty/CritiqueView.vue
@@ -8,6 +8,7 @@
           :items="semesters"
           item-value="id"
           item-title="title"
+          @update:modelValue="semesterSearchUpdate(selectedSemester)"
         ></v-select>
       </v-col>
     </v-row>
@@ -20,9 +21,40 @@
           :items="filteredCritiques"
           class="elevation-1"
         >
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>FILTER BY</v-toolbar-title>
+              <v-select
+                clearable
+                v-model="studentFilter"
+                label="Student"
+                :items="studentFilterArray"
+                :style="{ width: '70px' }"
+                @update:modelValue="filterCritiques()"
+              ></v-select>
+              <v-divider class="mx-4" inset vertical></v-divider>
+              <v-select
+                clearable
+                v-model="typeFilter"
+                label="Event Type"
+                :items="typeFilterArray"
+                :style="{ width: '70px' }"
+                @update:modelValue="filterCritiques()"
+              ></v-select>
+              <v-divider class="mx-4" inset vertical></v-divider>
+              <v-select
+                clearable
+                v-model="monthFilter"
+                label="Month"
+                :items="monthFilterArray"
+                :style="{ width: '70px' }"
+                @update:modelValue="filterCritiques()"
+              ></v-select>
+            </v-toolbar>
+          </template>
           <template #item="{ item }">
             <tr>
-              <td v-for="(header, index) in pg1stuHeaders" :key="index">
+              <td v-for="(header, index) in headers" :key="index">
                 <div v-if="header.title != ' '">
                   {{ item.columns[header.key] }}
                 </div>
@@ -50,12 +82,19 @@ export default {
   data: () => ({
     semesters: [],
     selectedSemester: null,
+    semesterCritiques: [],
     filteredCritiques: [],
+    studentFilterArray: [],
+    studentFilter: null,
+    typeFilterArray: [],
+    typeFilter: null,
+    monthFilterArray: [],
+    monthFilter: null,
     headers: [
-      { title: "Event Date", key: "1" },
-      { title: "First Name", key: "2" },
-      { title: "Last Name", key: "3" },
-      { title: "Event Type", key: "4" },
+      { title: "Event Date", key: "eventDate" },
+      { title: "First Name", key: "studentFName" },
+      { title: "Last Name", key: "studentLName" },
+      { title: "Event Type", key: "eventType" },
       { title: " " },
     ],
   }),
@@ -68,6 +107,8 @@ export default {
         .catch((e) => {
           console.log(e);
         });
+    },
+    async getCurrentSemester() {
       this.currentDate = new Date();
       let dateString = this.currentDate.toISOString().substring(0, 10);
       await SemesterDataService.getCurrent(dateString)
@@ -80,56 +121,55 @@ export default {
           console.log(e);
         });
     },
-    async retrieveAllEvents() {
-      await EventDataService.getAll()
+    async semesterSearchUpdate(semester) {
+      await EventDataService.getSemesterCritiques(semester)
         .then((response) => {
-          this.pg1events = response.data;
-          this.pg1events.reverse();
+          this.semesterCritiques = response.data;
+          this.semesterCritiques.forEach(
+            (obj) => (obj.stuName = obj.studentFName + " " + obj.studentLName)
+          );
+          this.filteredCritiques = this.semesterCritiques;
+          this.fillFilters();
         })
         .catch((e) => {
           console.log(e);
         });
     },
-    semesterSearchUpdate() {
-      if (
-        this.pg1selectedSemester === null ||
-        this.pg1selectedSemester === undefined
-      ) {
-        this.pg1filteredEvents = this.pg1events;
-      } else {
-        if (this.pg1selectedSemester.id !== undefined) {
-          this.pg1filteredEvents = this.pg1events.filter(
-            (obj) => obj.semesterId === this.pg1selectedSemester.id
-          );
-        }
-      }
-    },
-    async eventSearchUpdate() {
-      if (
-        this.pg1selectedEvent === null ||
-        this.pg1selectedEvent === undefined
-      ) {
-        this.critiques = [];
-      } else {
-        if (this.pg1selectedEvent.id !== undefined) {
-          await EventDataService.getCritiques(this.pg1selectedEvent.id)
-            .then((response) => {
-              this.pg1critiques = response.data;
-            })
-            .catch((e) => {
-              console.log(e);
-            });
-        }
-      }
-    },
     displayStudentCritiques(student) {
       this.console.log(student);
+    },
+    fillFilters() {
+      let set = new Set();
+      this.studentFilter = undefined;
+      this.semesterCritiques.forEach((obj) => set.add(obj.stuName));
+      this.studentFilterArray = Array.from(set);
+
+      set = new Set();
+      this.typeFilter = undefined;
+      this.semesterCritiques.forEach((obj) => set.add(obj.eventType));
+      this.typeFilterArray = Array.from(set);
+    },
+    filterCritiques() {
+      this.filteredCritiques = this.semesterCritiques;
+      if (this.studentFilter != undefined) {
+        this.filteredCritiques = this.filteredCritiques.filter(
+          (obj) => obj.stuName == this.studentFilter
+        );
+      }
+      if (this.typeFilter != undefined) {
+        this.filteredCritiques = this.filteredCritiques.filter(
+          (obj) => obj.eventType == this.typeFilter
+        );
+      }
     },
   },
   async mounted() {
     await this.retrieveAllSemesters();
-    await this.retrieveAllEvents();
     this.semesters.forEach((obj) => (obj.title = obj.year + " - " + obj.code));
+
+    await this.getCurrentSemester();
+    this.selectedSemester = this.semesters.find((obj) => obj.id == 15);
+    await this.semesterSearchUpdate(this.selectedSemester.id);
   },
   computed: {
     console: () => console,
