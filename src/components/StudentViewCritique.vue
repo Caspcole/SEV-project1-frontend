e6a8cdffa8cdf5245fd720de2156de2dcc4d408d
Alexandria Adams <alexm.adams@yahoo.com>
2023-03-17 16:29:22 -0500

    To have the simple changes I made exist in github

---
+++ src/components/StudentViewCritique.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container fluid class="bg-white">
+    <v-card>
+      <div class="text-subtitle-2 mt-4 mb-2">March 21 2023</div>
+
+      <v-expansion-panels variant="accordion">
+        <v-expansion-panel
+          v-for="i in 3"
+          :key="i"
+          title="Item"
+          text=""
+        ></v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "StudentViewCritique",
+  components: {},
+  data: () => ({}),
+};
+</script>
+
+<style scoped></style>
