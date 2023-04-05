91f9d2683db4d6b932cabcc883ae920a5a77d3ad
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-04-05 10:00:40 -0500

    basic page and navigation

---
+++ src/components/admin/AdminMaintainEvent.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card>
+    <v-card-title> Maintain Events: </v-card-title>
+  </v-card>
+  <v-container> </v-container>
+</template>
+<script>
+export default {
+  name: "adminMaintainEvent",
+  data: () => ({}),
+  methods: {},
+  watch: {},
+  computed: {},
+  async mounted() {},
+};
+</script>
