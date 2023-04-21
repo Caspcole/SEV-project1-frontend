ec2658cbbd6ba359119b74b023b0ea61466e443c
ThatGremlin <ben.johnson@eagles.oc.edu>
2023-04-21 12:16:56 -0500

    Revert "Merge branch 'dev' into cc-student-eventtime-availabilities"
    This reverts commit 63b90618cae04466b540ef3736dd8ff3bc768271, reversing
    changes made to 00a1c5fba6cb3d2f25da99aa4ce4320cc351f8a5.

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -58,7 +58,6 @@ export default defineComponent({
       userRoles: [],
       isExpanded: [],
       roleMenus: [],
-      userWithRoles: {},
       menus: [
         {
           title: "Student",
@@ -68,6 +67,10 @@ export default defineComponent({
             { link: "Event Sign-Ups", route: "studentEventSignUps" },
             { link: "View Your Critiques", route: "studentCritiques" },
             { link: "Event Recordings", route: "studentRecordings" },
+            {
+              link: "Vocal Levels",
+              route: "studentViewVocalLevelRequirements",
+            },
           ],
           id: 0,
         },
